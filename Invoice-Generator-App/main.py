1df8b321f7668f07b706adbf8d5558783f345d50
Omkar Patil <git21cs072-t@students.git.edu>
2024-06-29 10:45:15 +0530

    added Both projects

---
+++ Invoice-Generator-App/main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import glob
+from fpdf import FPDF
+from pathlib import Path
+
+filepaths = glob.glob("invoices/*.xlsx")
+print(filepaths)
+
+
+#read file paths and store into dataframe
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    #get invoice number
+    filename = Path(filepath).stem
+    invoiceSr = filename.split("-")[0]
+    #create Pdf
+    pdf  = FPDF(orientation="P", format="A4", unit="mm" )
+    pdf.add_page()
+    pdf.set_font(family="Times", size=18, style="B")
+    pdf.cell(w=50, h=8, txt="Invoice Sr : "+invoiceSr)
+    pdf.output("Example.pdf")
+
+
